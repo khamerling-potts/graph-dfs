2dd0d19553e331567c015d56a158e1562af5ae0b
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-01-18 09:40:09 -0500

    done

--- javascript/graph_dfs.js
+++ javascript/graph_dfs.js
@@ -1,15 +1,27 @@
-function isPath(graph, vertexA, vertexB) {
-  // type your code here
+function isPath(graph, vertexA, vertexB, visited = new Set()) {
+  // add vertexA to visited set
+  // for each vertex in graph[vertexa]:
+  //     if vertex is vertexb, return true
+  //     if vertex has not been visited and ispath(graph, vertex, vertexb): return true
+  // return false
+
+  visited.add(vertexA);
+  for (let vertex of graph[vertexA]) {
+    if (vertex === vertexB) return true;
+    if (!visited.has(vertex) && isPath(graph, vertex, vertexB, visited))
+      return true;
+  }
+  return false;
 }
 
 if (require.main === module) {
   // add your own tests in here
   let graph = {
     jan: ["john", "jambaby"],
-    john: ["carl"],
+    john: ["jan", "carl"],
     jambaby: [],
     carl: ["jambaby"],
-    dave: []
+    dave: [],
   };
 
   console.log("Expecting: true");
@@ -19,6 +31,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log(isPath(graph, "jan", "dave"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log(isPath(graph, "john", "jambaby"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log(isPath(graph, "carl", "carl"));
 }
 
 module.exports = isPath;
